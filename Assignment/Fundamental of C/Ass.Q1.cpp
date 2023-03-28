53e5802d2c315f391ddec5122b8cf3624a3c0eff
Bhavik1605 <bhavikgohil777@gmail.com>
2023-03-28 18:30:20 +0530

    delete

--- Assignment/Fundamental of C/Ass.Q1.cpp
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-
-main()
-{
-	printf("1. My Name : Bhavik Gohil \n");
-	printf("2. My Birthdate : 16/05/1998 \n");
-    printf("3. My Age : 23 \n");	
-    printf("2. My Address : Out of Shiyani Pol,Ram Society,Wadhwan city, Surendranagar.");
-}
