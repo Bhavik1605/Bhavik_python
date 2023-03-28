6cf4556132abab56efc12ec10b549155250dae82
Bhavik1605 <bhavikgohil777@gmail.com>
2023-03-28 18:26:28 +0530

    first program

---
+++ Assignment/Fundamental of C/Ass.Q4(S.I).cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+	//Simple Interest Program
+	float Principal,Rate,Time,Simple_Interest;
+	
+	printf("Enter The Principal : ");
+	scanf("%f",&Principal);
+	
+	printf("Enter The Rate : ");
+	scanf("%f",&Rate);
+	
+	printf("Enter The Time : ");
+	scanf("%f",&Time);
+	
+	Simple_Interest = Principal*Rate*Time / 100;
+	
+	printf("The Simple Interest = %f",Simple_Interest);
+}
