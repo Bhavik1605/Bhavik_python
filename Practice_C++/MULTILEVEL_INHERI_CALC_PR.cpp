e6c38eb070f3f378e58b5408b72f1aedbe94a09d
Bhavik1605 <bhavikgohil777@gmail.com>
2023-06-28 19:14:30 +0530

    Practice_C++

---
+++ Practice_C++/MULTILEVEL_INHERI_CALC_PR.cpp
@@ -0,0 +1,76 @@
+// calculate the percentage of a student using multi-level inheritance.
+
+#include<iostream>
+using namespace std;
+
+class student{
+	protected:
+		string name;
+		int roll,sub[2];
+	public:
+		void get_data()
+		{
+			fflush(stdin);
+			
+			cout<<"Enter Your Name : ";
+			getline(cin,name);
+			cout<<"Enter Your Roll Number : ";
+			cin>>roll;
+			cout<<"Enter Mark of Your Maths Subject : ";
+			cin>>sub[0];
+			cout<<"Enter Mark of Your Science Subject : ";
+			cin>>sub[1];
+			cout<<"Enter Mark of Your English Subject : ";
+			cin>>sub[2];
+			
+			if(sub[0]  >100 || sub[1]  >100 || sub[2] >100)
+			{
+				cout<<endl<<"Please Enter Correct Marks...";
+				exit(0);
+			}
+			
+			else if(sub[0]  <33 || sub[1]  <33 || sub[2] <33)
+			{
+				cout<<endl<<"Sorry..You are Fail !!! Better Luck Next Time....";
+				exit(0);
+			}
+		}
+};
+class Total:public student{
+	protected:
+		int total;
+	public:
+		void sum()
+		{
+			total = sub[0]+sub[1]+sub[2];
+		}
+};
+
+class Per:public Total{
+	private:
+		float pr;
+	public:
+		void per()
+		{
+			pr = (total/3.0);
+		}
+		void show()
+		{
+			cout<<endl<<"---------------Your_Result-----------------"<<endl;
+			cout<<endl<<"Your Name is : "<<name;
+			cout<<endl<<"Your Roll Number is : "<<roll<<endl;
+			cout<<endl<<"Your Total Percentage is : "<<pr;
+		}
+};
+
+int main()
+{
+	Per p;
+	
+	p.get_data();
+	p.sum();
+	p.per();
+	p.show();
+	
+	return 0;
+}
