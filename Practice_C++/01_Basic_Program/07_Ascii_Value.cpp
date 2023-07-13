e7af49caa523de88d6474353c202edbb74784575
Bhavik1605 <bhavikgohil777@gmail.com>
2023-07-13 15:13:14 +0530

    Practice

---
+++ Practice_C++/01_Basic_Program/07_Ascii_Value.cpp
@@ -0,0 +1,16 @@
+/*  C++ Program to Display ASCII Value of a Character  */
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char c;
+	
+	cout<<"Enter any Character : ";
+	cin>>c;
+	
+	cout<<endl<<"The ASCII Value of The Character is : "<<int(c)<<endl;
+
+	return 0;
+}
