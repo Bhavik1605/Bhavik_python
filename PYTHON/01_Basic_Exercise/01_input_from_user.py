7c935dad90222655a93337c597324c3f1d241ace
Bhavik1605 <bhavikgohil777@gmail.com>
2023-08-12 11:46:40 +0530

    Practice

---
+++ PYTHON/01_Basic_Exercise/01_input_from_user.py
@@ -0,0 +1,7 @@
+''' Write a program to accept two numbers from the user and calculate multiplication '''
+
+
+num1=int(input("Enter First Number : "))
+num2=int(input("Enter Second Number : "))
+
+print("Multiplication of Two Number is : ",num1*num2)
