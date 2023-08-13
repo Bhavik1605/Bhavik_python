bb53db51847d682eecc61d1a13946ade59cab959
Bhavik1605 <bhavikgohil777@gmail.com>
2023-08-13 16:47:52 +0530

    Practice_C++

---
+++ PYTHON/01_Basic_Exercise/03_Basic_Calculation.py
@@ -0,0 +1,8 @@
+# Accept two numbers from the user and return their sum, multiplication and division.
+
+num1=int(input("Enter First Number : "))
+num2=int(input("Enter Second Number : "))
+
+print("\nSum of Two Number is : ",num1+num2)
+print("\nMultiplication of Two Number is : ",num1*num2)
+print("\nDivision of Two Number is : ",num1/num2)
