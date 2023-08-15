26e89de74502372d0bcd762e18ba5fa3540f5e3c
Bhavik1605 <bhavikgohil777@gmail.com>
2023-08-15 10:56:11 +0530

    Prectice

---
+++ PYTHON/01_Basic_Exercise/05_Even_odd.py
@@ -0,0 +1,12 @@
+'''
+Write a Python program that determines whether a given number(accepted from the user)
+is even or odd, and prints an appropriate message to the user.
+'''
+
+num=int(input("Enter A Numbr : "))
+
+if num%2==0:
+    print(num,"is Even Number")
+
+else:
+    print(num,"is Odd Number")
