5c8c3f847ea7c469f3724489b262f3657a9950c7
Bhavik1605 <bhavikgohil777@gmail.com>
2023-08-17 13:59:21 +0530

    Practice

---
+++ PYTHON/02_Conditional_Statement/01_if_else.py
@@ -0,0 +1,9 @@
+#Check whether a Person is Eligible for Voting or Not
+
+age=int(input("Enter Your Age : "))
+
+if age>=18:
+    print("\nYou are Eligible for Voting")
+
+else:
+    print("\nYou are  Not Eligible for Voting")
