3d512bed4df000ffa6c0c69de8ae29e7a5f4a58d
Bhavik1605 <bhavikgohil777@gmail.com>
2023-08-26 13:46:37 +0530

    Python_Practice

---
+++ PYTHON/03_Loop_Exercise/10_count_Even_odd_in_list.py
@@ -0,0 +1,10 @@
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+odd = 0
+even = 0
+for x in l:
+        if x % 2 == 0:
+    	     even+=1
+        else:
+    	     odd+=1
+print("Number of even numbers :",even)
+print("Number of odd numbers :",odd)
