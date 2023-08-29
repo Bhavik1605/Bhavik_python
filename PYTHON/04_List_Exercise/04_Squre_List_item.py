0ebed5ea4a0721a96ba40b6fc0f00c37270601e2
Bhavik1605 <bhavikgohil777@gmail.com>
2023-08-29 11:26:01 +0530

    Practice_Python

---
+++ PYTHON/04_List_Exercise/04_Squre_List_item.py
@@ -0,0 +1,10 @@
+# Turn every item of a list into its square.
+
+L = [1, 2, 3, 4, 5, 6, 7]
+res = []
+for i in L:
+    res.append(i * i)
+
+print("Orignal List is : ",L)
+print("Squre of List Item is : " ,end="")
+print(res)
