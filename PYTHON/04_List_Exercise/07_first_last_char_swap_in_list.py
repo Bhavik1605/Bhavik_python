0afe598ca095a0bd54c6946c94d8ffa1f86ea039
Bhavik1605 <bhavikgohil777@gmail.com>
2023-08-30 21:26:57 +0530

    Python_Practice

---
+++ PYTHON/04_List_Exercise/07_first_last_char_swap_in_list.py
@@ -0,0 +1,9 @@
+# Write a Python Program to Interchange First and Last Elements of in a List
+
+L1 = ['XYZ', 'ABC', 'xyz', 'abc']
+
+print("Original List :",L1)
+ 
+L1[0], L1[-1] = L1[-1], L1[0]
+ 
+print("List after swapping :",L1)
