db73c242c8133320f0d8bc2c2a64100cc740981a
Bhavik1605 <bhavikgohil777@gmail.com>
2023-09-02 10:42:17 +0530

    Python_Practice

--- PYTHON/05_Function_Exercise/03_Reverse_String_without_using_function.py
+++ PYTHON/05_Function_Exercise/03_Reverse_String_without_using_function.py
@@ -1,3 +1,5 @@
+# Write a Python program to reverse a string.
+
 def reverse(s):
 
     rev = ""
---
+++ PYTHON/05_Function_Exercise/04_Factorial_of_given_Num.py
@@ -0,0 +1,16 @@
+'''
+Write a Python function to calculate the factorial of a number.
+The function accepts the number as an argument.
+'''
+
+def fact(n):
+    if n < 0:
+        return "Factorial is undefined for negative numbers"
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        return n * fact(n - 1)
+
+n=int(input("Input a Number to Compute The Factiorial : "))
+res = fact(n)
+print(f"The factorial of {n} is {res}")
