70a05323facb368532e717e923f4097de10ac54a
Bhavik1605 <bhavikgohil777@gmail.com>
2023-09-09 19:11:13 +0530

    Python_Practice

---
+++ PYTHON/06_Dictionary_Exercise/03_itrate_dictionary_item.py
@@ -0,0 +1,6 @@
+#  Write a program to iterate over dictionary items using for loop.
+
+d = {0:"Bhavik", 1:"Kuldeep", 2:"Rohit"}
+
+for key, val in d.items():
+    print(f" Value of key {key} is {val}")
