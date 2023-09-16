ed1dd7ac8ea994d39304723a1eefeeec0c490cb8
Bhavik1605 <bhavikgohil777@gmail.com>
2023-09-16 17:13:26 +0530

    Python_Practice

---
+++ PYTHON/07_Array_Exercise/01_Sum_of_Array.py
@@ -0,0 +1,8 @@
+#Calculate the sum of elements in an array using python
+
+arr = [10, 89, 9, 56, 4, 80, 8]
+Sum = 0
+
+for i in range(len(arr)):
+   Sum = Sum + arr[i]
+print("Sum of the array is :", Sum)
