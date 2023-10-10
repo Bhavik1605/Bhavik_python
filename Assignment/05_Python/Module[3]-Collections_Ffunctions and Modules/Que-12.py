0d29358c6a8629ddcbbd8c05c6a606199258043f
Bhavik1605 <bhavikgohil777@gmail.com>
2023-10-10 13:17:04 +0530

    Assignment_Python

---
+++ Assignment/05_Python/Module[3]-Collections_Ffunctions and Modules/Que-12.py
@@ -0,0 +1,8 @@
+# Write a Python program to convert a list of characters into a string. 
+
+
+char_list = ['H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd']
+
+result_string = ''.join(char_list)
+
+print(result_string)
