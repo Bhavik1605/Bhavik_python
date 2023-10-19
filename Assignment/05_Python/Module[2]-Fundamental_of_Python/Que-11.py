42bfd14e77749b28791c416ea9fd65fd4259e4c4
Bhavik1605 <bhavikgohil777@gmail.com>
2023-10-19 10:55:56 +0530

    Assignment_Python

---
+++ Assignment/05_Python/Module[2]-Fundamental_of_Python/Que-11.py
@@ -0,0 +1,11 @@
+# Write a python program to sum of the first n positive integers.
+
+n = int(input("Enter a Positive Integer : "))
+    
+if n <= 0:
+    print("Please Enter a Positive Integer.")
+else:
+    sum = 0
+    for i in range(1, n + 1):
+        sum += i
+    print(f"The Sum of The First {n} Positive Integers is : {sum}")
