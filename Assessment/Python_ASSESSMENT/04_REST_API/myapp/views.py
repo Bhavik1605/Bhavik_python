6bd4fa7ca23b54a5e05e12a05c5f3a6aad8cace9
Bhavik1605 <bhavikgohil777@gmail.com>
2023-12-03 21:31:34 +0530

    ASSESSMENT_API

---
+++ Assessment/Python_ASSESSMENT/04_REST_API/myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Snippet
+from .serializers import SnippetSerializers
+
+class SnippetList(generics.ListCreateAPIView):
+	queryset=Snippet.objects.all()
+	serializer_class=SnippetSerializers
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+	queryset=Snippet
+	serializer_class=SnippetSerializers
+
